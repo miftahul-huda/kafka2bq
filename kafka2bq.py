9f12134f15e9e4a6a3370dd28ec921d4346fe7e6
Miftahul Huda <56541336+miftahul-huda@users.noreply.github.com>
2020-10-02 16:09:00 +0700

    Add files via upload

---
+++ kafka2bq.py
@@ -0,0 +1,232 @@
+from confluent_kafka import Consumer, KafkaError
+from confluent_kafka import TopicPartition
+import os
+import subprocess
+import sys
+import argparse
+import numpy as np
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import json
+from datetime import datetime
+import time
+
+class Empty:
+    configuration= None
+
+
+class KafkaConsumer:
+    settings = {
+        'bootstrap.servers': '',
+        'group.id': '',
+        'client.id': '',
+        'enable.auto.commit': True,
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'smallest'}
+    }
+
+    messageCounter= 0
+
+    buffer=[]
+
+    doneWaiting =  False
+
+    startTime = None
+
+    def read_config_from_file(self, config_file):
+        file = config_file
+        f = open(file, "r")
+        lines = f.readlines()
+
+        o = self.parse_config(lines)
+        self.settings['bootstrap.servers'] = o.bootstrapServer
+        self.settings['group.id'] = o.groupId
+        self.settings['client.id'] = o.clientId
+
+        return o
+
+    def parse_config(self, lines):
+        o = Empty()
+        o.partition = 0
+        o.credential =  None
+
+        counter = 0
+        while counter < len(lines):
+            line = lines[counter]
+            if("#" in line or ""):
+                counter = counter + 1
+                continue
+
+            elif("-bootstrap-server " in line):
+                s = line.replace("-bootstrap-server ", "")
+                s = s.strip()
+                o.bootstrapServer = s
+            elif("-group-id " in line):
+                s = line.replace("-group-id ", "")
+                s = s.strip()
+                o.groupId = s
+            elif("-partition " in line):
+                s = line.replace("-partition ", "")
+                s = s.strip()
+                o.partition = int(s)
+
+            elif("-client-id " in line):
+                s = line.replace("-client-id ", "")
+                s = s.strip()
+                o.clientId = s
+
+            elif("-credential " in line):
+                s = line.replace("-credential ", "")
+                s = s.strip()
+                o.credential = s
+
+            elif("-kafka-topic " in line):
+                s = line.replace("-kafka-topic ", "")
+                s = s.strip()
+                o.kafkaTopic = s
+
+            elif("-project " in line):
+                s = line.replace("-project ", "")
+                s = s.strip()
+                o.project = s
+
+            elif("-output-bq-table " in line):
+                s = line.replace("-output-bq-table ", "")
+                s = s.strip()
+                o.outputBqTable = s
+
+            elif("-rows-insert-num " in line):
+                s = line.replace("-rows-insert-num ", "")
+                s = s.strip()
+                o.rowsInsertNum = int(s)
+
+            elif("-max-time-waiting " in line):
+                s = line.replace("-max-time-waiting ", "")
+                s = s.strip()
+                o.maxTimeWaiting = int(s)
+
+            counter = counter + 1
+        return o
+
+
+    def get_client(self, key_path=None, project_id=None):
+        if(key_path is None):
+            client = bigquery.Client(project_id)
+        else:
+            credentials = service_account.Credentials.from_service_account_file(
+                    key_path,
+                    scopes=["https://www.googleapis.com/auth/cloud-platform"],
+            )
+            client = bigquery.Client(
+                    credentials=credentials,
+                    project=credentials.project_id,
+            )
+        return client
+
+    def process_to_bq(self, msg, config):
+
+        if(msg is not None):
+            now = datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
+            msg = msg.replace("NOW()", now)
+            item = json.loads(msg )
+            #print(item)
+            self.buffer.append(item)
+            self.messageCounter = self.messageCounter + 1
+
+        if((self.messageCounter > config.rowsInsertNum and len(self.buffer) > 0)) :
+            self.save(config)
+        else:
+            if(msg is None):
+                self.save(config)
+
+    def save(self,  config):
+        if(len(self.buffer) > 0):
+            print("Saving to " + config.outputBqTable)
+            startTime = datetime.now()
+            #self.print_all(self.buffer)
+            errors = self.save_data(self.buffer, config)
+            errors = []
+            print("Total rows  : " + str(len( self.buffer )))
+
+            if(len(errors) == 0):
+                self.messageCounter = 0
+                self.buffer = []
+                endtime = datetime.now()
+                print("Data has been saved in " + str(endtime - startTime))
+                print("\n\n")
+            else:
+                print("Error saving to BigQuery")
+                print(errors)
+                print("Waiting for 3 seconds...")
+                time.sleep(3)
+
+    def print_all(self, arr):
+        for item in arr:
+            print(item)
+
+    def save_data(self, result, config):
+
+        project_id = config.project
+        credential = config.credential
+        table_id = config.outputBqTable
+        client = self.get_client(credential, project_id)
+
+        table = client.get_table(table_id)  # API request
+        errors = client.insert_rows(table, result)  # API request
+        return errors
+
+
+
+    def process(self, config):
+        c = Consumer(self.settings)
+        c.subscribe([config.kafkaTopic])
+
+        #topic_partition = TopicPartition(config.kafkaTopic, config.partition)
+        #c.assign([topic_partition])
+
+        try:
+            while True:
+                msg = c.poll(0.1)
+                if msg is None:
+
+                    self.process_to_bq(None, config)
+
+                    continue
+                elif not msg.error():
+                    #dt_object = datetime.fromtimestamp(float(msg.timestamp()))
+                    print('Received message at: {0}'.format(msg.timestamp()))
+                    #print(msg.value())
+                    self.process_to_bq(msg.value(), config)
+
+                elif msg.error().code() == KafkaError._PARTITION_EOF:
+                    print('End of partition reached {0}/{1}'
+                          .format(msg.topic(), msg.partition()))
+                else:
+                    print('Error occured: {0}'.format(msg.error().str()))
+
+        except KeyboardInterrupt:
+            pass
+
+        finally:
+            c.close()
+
+    def start(self, description):
+        parser = argparse.ArgumentParser(description=description)
+        required = parser.add_argument_group("Required argument")
+        required.add_argument('-f', '--file', type=str, help="configuration filename", required=True)
+        args = parser.parse_args()
+        o = self.read_config_from_file(args.file)
+        print("====================================")
+        print("Kafka bootstrap : " + o.bootstrapServer )
+        print("Kafka Topic : " + o.kafkaTopic )
+        print("Topic partition : " + str(o.partition) )
+        print("Client Group : " + o.groupId )
+        print("Client id : " + o.clientId )
+        print("BigQuery table : " + o.outputBqTable )
+        print("====================================")
+        print("Starting Kafka to BigQuery...")
+        self.process(o)
+
+if __name__ == "__main__":
+    o = KafkaConsumer()
+    o.start("Consume Kafka to BigQuery")
---
+++ server.py
@@ -0,0 +1,565 @@
+import os
+import flask
+import base64
+import random
+import string
+import subprocess
+import signal
+import json
+import thread
+import copy
+from subprocess import PIPE
+from flask import render_template
+
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+host = '0.0.0.0'
+port = 5555
+
+
+def check_processes(name, num):
+    for item in processes:
+        print("check_processes")
+        proc = item.process
+        print(proc)
+        try:
+            print("Readline")
+            text = proc.stdout.read()
+            print(item.clientId + " : " + text)
+        except Exception, e:
+            print("Error  check_processes")
+            print(e)
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+def get_random_number(length):
+    letters = '1234567890'
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+def check_exists(arr, file):
+    print("check_exists")
+    for item in arr:
+        if item.file == file:
+            return True
+
+    return False
+
+def get_pid(arr, file):
+
+    for item in arr:
+        if item.file == file:
+            return item.pid
+
+    return None
+
+def get_process(arr, file):
+
+    for item in arr:
+        if item.file == file:
+            return item
+
+    return None
+
+def get_processes_by_folder(arr, folder):
+    items  = []
+    for item in arr:
+        if item.folder == folder:
+            items.append(item)
+
+    return items
+
+def remove(arr, file):
+    idx = -1
+    counter = 0
+    for item in arr:
+        if item.file == file:
+            idx = counter
+            break
+        counter = counter + 1
+    if  idx  >  -1:
+        arr.pop(idx)
+    return arr
+
+def parse_folder(folder):
+    ss = folder.split("_")
+    host = ss[0]
+    sss  = ss[1].split("/")
+    port =  sss[0]
+
+    ss = folder.split("/")
+    ss  = ss[1]
+    ssss  =  ss.split("_")
+    group  =  ssss[0]
+    topic = ssss[1]
+
+    return [host, port, group, topic]
+
+
+def obj_dict(obj):
+    return obj.__dict__
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Hello World"
+
+@app.route('/hello/<name>', methods=['GET'])
+def helloname(name):
+    return "Hello, " + name
+
+@app.route('/web')
+def static_file():
+    return render_template('index.html')
+
+@app.route('/web/new')
+def new():
+    return render_template('new.html')
+
+@app.route('/web/view/<id>')
+def view(id):
+    return render_template('view.html', id=id)
+
+processes = []
+@app.route('/kafka2bq/start/<server>/<port>/<group>/<topic>/<project>/<table>/<num>/<credential>', methods=['GET'])
+def start(server, port, group, topic, project, table, num, credential):
+    info = []
+    os.system("mkdir " + server + "_" + port + "" )
+    folder = server + "_" + port + "/" + group + "_" + topic
+    id  = base64.encodestring(folder)
+    os.system("mkdir " + folder )
+    credential = base64.decodestring(credential)
+    os.path.dirname(os.path.realpath(__file__))
+    s = ("#Credential for accessing BigQuery"
+        "\n-credential " + credential + ""
+        "\n\n#Kafka topic "
+        "\n-kafka-topic " + topic + ""
+        "\n\n#BigQuery project "
+        "\n-project " + project + ""
+        "\n\n#BigQuery  table output "
+        "\n-output-bq-table " + table + ""
+        "\n\n#Maximum rows to insert "
+        "\n-rows-insert-num " + num + ""
+        "\n\n# Kafka consumer settings "
+        "\n-bootstrap-server " + server + ":" + port + ""
+        "\n-group-id " + group + ""
+        "\n-client-id <clientId>\n")
+
+
+    for i in range(int(num)):
+        clientId = 'Client-' + get_random_number(5)
+        ss  = s.replace('<clientId>', clientId)
+        file = folder + "/" + clientId + '.conf'
+        f = open(file, "wt")
+        f.write(ss)
+        f.close()
+
+
+        if check_exists(processes, file)  == True:
+            print("\n\nConsumer " + file + " Exists!\nRestarting...\n")
+            pid = get_pid(processes, file)
+            if pid is not None:
+                try:
+                    print("\n\nKill " + str(pid))
+                    os.kill(pid, signal.SIGTERM) #or signal.SIGKILL
+                except:
+                    print("Already killed")
+
+            remove(processes, file)
+
+        print("Starting " + file)
+        proc = subprocess.Popen(["python", "kafka2bq.py", "-f" + file])
+        print("proc")
+        print(proc)
+        o = lambda: None
+        o.id = id.replace("\n",  "")
+        o.pid = proc.pid
+        o.folder = folder
+        o.file = file
+        o.clientId = clientId
+        o.process = proc
+        o.status = 1
+        o.table = table
+
+        o.configuration = s
+
+        processes.append(o)
+        print("File "  + file + ", PID : " + str(o.pid))
+        #info = info + o.clientId + ", PID: " + str(o.pid) +"\n"
+
+        info.append(o)
+
+    """try:
+       thread.start_new_thread( check_processes, ("Thread-1", 2, ))
+    except Exception, e:
+       print "Error: unable to start thread"
+       print(e)"""
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/add/<server>/<port>/<group>/<topic>/<num>', methods=['GET'])
+def addclients(server, port, group, topic, num):
+    info = []
+    error =  lambda: None
+
+    os.path.dirname(os.path.realpath(__file__))
+    folder = server + "_" + port + "/" + group + "_" + topic
+    id  = base64.encodestring(folder)
+    items = get_processes_by_folder(processes, folder)
+
+    if len(items) == 0:
+        error.message = "No existing group  and topic "
+        sjson = json.dumps(error, default=obj_dict)
+        return sjson
+
+    start = len(items)
+    end  = start +  int(num)
+
+    itemtemplate = items[0]
+    s = itemtemplate.configuration
+
+    for i in range(start, end):
+        clientId = 'Client-' + + get_random_number(5)
+        ss  = s.replace('<clientId>', clientId)
+        file = folder + "/" + clientId + '.conf'
+        f = open(file, "wt")
+        f.write(ss)
+        f.close()
+
+        print("Starting " + file)
+        proc = subprocess.Popen(["python", "kafka2bq.py", "-f" + file])
+
+        o = lambda: None
+        o.id = id.replace("\n",  "")
+        o.pid = proc.pid
+        o.folder = folder
+        o.file = file
+        o.clientId = clientId
+        o.process = proc
+        o.configuration = s
+        o.status = 1
+        o.table = itemtemplate.table
+        processes.append(o)
+        print("File "  + file + ", PID : " + str(o.pid))
+        #info = info + o.clientId + ", PID: " + str(o.pid) +"\n"
+        info.append(o)
+
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/add/<id>/<num>', methods=['GET'])
+def addclientsbyid(id, num):
+    info = []
+    error =  lambda: None
+
+    os.path.dirname(os.path.realpath(__file__))
+    folder = base64.decodestring(id)
+    items = get_processes_by_folder(processes, folder)
+
+    if len(items) == 0:
+        error.message = "No existing group  and topic "
+        sjson = json.dumps(error, default=obj_dict)
+        return sjson
+
+    start = len(items)
+    end  = start +  int(num)
+
+    itemtemplate = items[0]
+    s = itemtemplate.configuration
+
+    for i in range(start, end):
+        clientId = 'Client-' + get_random_number(5)
+        ss  = s.replace('<clientId>', clientId)
+        file = folder + "/" + clientId + '.conf'
+        f = open(file, "wt")
+        f.write(ss)
+        f.close()
+
+        print("Starting " + file)
+        proc = subprocess.Popen(["python", "kafka2bq.py", "-f" + file])
+
+        o = lambda: None
+        o.id = id.replace("\n",  "")
+        o.pid = proc.pid
+        o.folder = folder
+        o.file = file
+        o.clientId = clientId
+        o.process = proc
+        o.configuration = s
+        o.status = 1
+        o.table = itemtemplate.table
+        processes.append(o)
+        print("File "  + file + ", PID : " + str(o.pid))
+        #info = info + o.clientId + ", PID: " + str(o.pid) +"\n"
+        info.append(o)
+
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/status/<server>/<port>/<group>/<topic>', methods=['GET'])
+def status(server, port, group, topic):
+    info = []
+    folder = server + "_" + port + "/" + group + "_" + topic
+    for item in processes:
+        if item.file.startswith(folder):
+            p = item.process
+            if p.poll() == None:
+                #info = info + item.clientId + ", PID: " + str(item.pid) + " is running\n"
+                item.status = 1
+            else:
+                item.status = 0
+            print(item.clientId)
+            info.append(item)
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/status/<id>', methods=['GET'])
+def statusbyid(id):
+    folder = base64.decodestring(id)
+
+
+    info = []
+    for item in processes:
+        if item.file.startswith(folder):
+            p = item.process
+            if p.poll() == None:
+                #info = info + item.clientId + ", PID: " + str(item.pid) + " is running\n"
+                item.status = 1
+            else:
+                item.status = 0
+            print(item.clientId)
+            info.append(item)
+
+    inf = lambda: None
+    if(len(info) > 0):
+        inf.id = info[0].id.replace("\n",  "")
+        inf.table = info[0].table
+        folder =  parse_folder(info[0].folder)
+        inf.server = folder[0]
+        inf.port = folder[1]
+        inf.group = folder[2]
+        inf.topic = folder[3]
+        inf.workers = info
+
+    sjson = json.dumps(inf, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/status/<server>/<port>/<group>/<topic>/<clientid>', methods=['GET'])
+def clientstatus(server, port, group, topic, clientid):
+    info = []
+    folder = server + "_" + port + "/" + group + "_" + topic
+    for item in processes:
+        print (item.clientId + " === " + clientid)
+        if item.clientId == clientid:
+            p = item.process
+            if p.poll() == None:
+                #info = info + item.clientId + ", PID: " + str(item.pid) + " is running\n"
+                item.status = 1
+            else:
+                item.status = 0
+            info = item
+            break
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/sessions', methods=['GET'])
+def sessions():
+    info = []
+    ids =  []
+    for item in processes:
+        if(item.id.replace("\n",  "") not in ids):
+            ids.append(item.id.replace("\n",  ""))
+            o = lambda: None
+            o.id = item.id.replace("\n",  "")
+            o.folder =  item.folder
+            oo  =  parse_folder(item.folder)
+            o.server = oo[0]
+            o.port  =  oo[1]
+            o.group = oo[2]
+            o.topic = oo[3]
+            o.table = item.table
+            info.append(o)
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+
+@app.route('/kafka2bq/restart/<server>/<port>/<group>/<topic>/<clientid>', methods=['GET'])
+def restartclient(server, port, group, topic, clientid):
+    info = []
+    folder = server + "_" + port + "/" + group + "_" + topic
+    for item in processes:
+        print (item.clientId + " === " + clientid)
+        if item.clientId == clientid:
+            pid =  item.pid
+            try:
+                print("\n\nKill " + str(pid))
+                os.kill(pid, signal.SIGTERM) #or signal.SIGKILL
+            except:
+                print("Already killed")
+            remove(processes, item.file)
+
+            print("\nRestarting " + item.file)
+            print("\n\n")
+            proc = subprocess.Popen(["python", "kafka2bq.py", "-f" + item.file])
+            o = lambda: None
+            o.id = item.id.replace("\n",  "")
+            o.pid = proc.pid
+            o.folder = item.folder
+            o.file = item.file
+            o.clientId = item.clientId
+            o.process = proc
+            o.configuration = item.configuration
+            o.status = 1
+            print("New instance PID: " + str(o.pid))
+            processes.append(o)
+            info = o
+            break
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/restart/<id>/<clientid>', methods=['GET'])
+def restartclientbyid(id, clientid):
+    info = []
+    folder = base64.decodestring(id);
+    for item in processes:
+        print (item.clientId + " === " + clientid)
+        if item.clientId == clientid:
+            pid =  item.pid
+            try:
+                print("\n\nKill " + str(pid))
+                os.kill(pid, signal.SIGTERM) #or signal.SIGKILL
+            except:
+                print("Already killed")
+
+            remove(processes, item.file)
+
+            print("\nRestarting " + item.file)
+            print("\n\n")
+            proc = subprocess.Popen(["python", "kafka2bq.py", "-f" + item.file])
+            o = lambda: None
+            o.id = item.id.replace("\n",  "")
+            o.pid = proc.pid
+            o.folder = item.folder
+            o.file = item.file
+            o.clientId = item.clientId
+            o.process = proc
+            o.configuration = item.configuration
+            o.status = 1
+            print("New instance PID: " + str(o.pid))
+            processes.append(o)
+            info = o
+            break
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/stop/<server>/<port>/<group>/<topic>/<clientid>', methods=['GET'])
+def stopclient(server, port, group, topic, clientid):
+    info = []
+    folder = server + "_" + port + "/" + group + "_" + topic
+    for item in processes:
+        print (item.clientId + " === " + clientid)
+        if item.clientId == clientid:
+            pid =  item.pid
+            try:
+                print("\n\nKill " + str(pid))
+                os.kill(pid, signal.SIGTERM) #or signal.SIGKILL
+            except:
+                print("Already killed")
+
+            remove(processes, item.file)
+            item.status  =  0
+            info = item
+            break
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/stop/<id>/<clientid>', methods=['GET'])
+def stopclientbyid(id, clientid):
+    info = []
+    folder =  base64.decodestring(id);
+    for item in processes:
+        print (item.clientId + " === " + clientid)
+        if item.clientId == clientid:
+            pid =  item.pid
+            try:
+                print("\n\nKill " + str(pid))
+                os.kill(pid, signal.SIGTERM) #or signal.SIGKILL
+            except:
+                print("Already killed")
+            remove(processes, item.file)
+            item.status  =  0
+            info = item
+            break
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/stop/<server>/<port>/<group>/<topic>', methods=['GET'])
+def stopallgrouptopicclient(server, port, group, topic):
+    info = []
+    removed  = []
+    folder = server + "_" + port + "/" + group + "_" + topic
+
+    for item in processes:
+        print(item.folder + " == " + folder)
+        if item.folder == folder:
+            pid =  item.pid
+            try:
+                print("\n\nKill " + str(pid))
+                os.kill(pid, signal.SIGTERM) #or signal.SIGKILL
+            except:
+                print("Already killed")
+
+            item.status  =  0
+            removed.append(item)
+            info.append(item)
+
+    for item in removed:
+        remove(processes, item.file)
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+@app.route('/kafka2bq/stop/<id>', methods=['GET'])
+def stopallbyid(id):
+    info = []
+    removed  = []
+    folder = base64.decodestring(id)
+
+    for item in processes:
+        print(item.folder + " == " + folder)
+        if item.folder == folder:
+            pid =  item.pid
+            try:
+                print("\n\nKill " + str(pid))
+                os.kill(pid, signal.SIGTERM) #or signal.SIGKILL
+            except:
+                print("Already killed")
+
+            item.status  =  0
+            removed.append(item)
+            info.append(item)
+
+    for item in removed:
+        remove(processes, item.file)
+
+    sjson = json.dumps(info, default=obj_dict)
+    return sjson
+
+
+print("Running kafka2bq server on " + host + ":"  + str(port))
+app.run(host, port)
